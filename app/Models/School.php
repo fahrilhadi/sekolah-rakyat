1f86abdaab0f74d22a50089f484d980c0517df95
Fahril Hadi <fahrilhadi77@gmail.com>
2026-09-12 19:34:48 +0700

    create model and migration school

---
+++ app/Models/School.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class School extends Model
+{
+    protected $fillable = [
+        'name',
+        'code',
+        'slug',
+        'address',
+        'phone',
+        'email',
+        'logo_path',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_active' => 'boolean',
+        ];
+    }
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+}
